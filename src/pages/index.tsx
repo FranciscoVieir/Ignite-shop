42850dce6a44ce26f74527c352cb917fa173a9ec
FranciscoVieir <franciscovieira6397@gmail.com>
2023-02-25 18:06:39 -0300

    feat: Formatting the price of products for Brazilian real

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount! / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount! / 100),
     }
   })
 
@@ -65,6 +68,6 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       products,
     },
-    revalidate: 60 * 60 * 2,
+    revalidate: 60 * 60 * 2, // 2 horas
   }
 }
